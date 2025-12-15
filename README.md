a01ab1a69a4329150469a388e97da244d4553db9
Alexander Jandt <kurelen@pm.me>
2025-12-15 13:16:46 +0100

    Linting, formatting, fixing

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-# Quick and dirty PCP solver
+# PCP solver
 
-Small javascript doodle to find solutions to some pcp instanes.
+Small javascript doodle to find solutions to some pcp instances.
 
-Usage: node pcp.js [instance] [start_budget] [[end_budget] [incrementer]]
-With
-    [instance]     in 1, 2, 3
-    [start_budget] non-negative number
-    [end_budget]   non-negative number greater than [start_budget]
-    [incrementer]  non-negative number
+Usage:
+`node pcp.js [instance] [start_budget] [[end_budget] [incrementer]]`
+
+With:
+
+- `[instance]` in 1, 2, 3
+- `[start_budget]` non-negative number
+- `[end_budget]` non-negative number greater than [start_budget]
+- `[incrementer]` non-negative number
--- pcp.js
+++ pcp.js
@@ -1,12 +1,12 @@
 const instance_1 = [
   ["111", "110"],
-  ["011","1"],
+  ["011", "1"],
   ["10", "100"],
-  ["0", "11"]
+  ["0", "11"],
 ];
 
 const instance_2 = [
-  ["0","111"],
+  ["0", "111"],
   ["11", "0"],
   ["10", "100"],
   ["000", "0"],
@@ -14,7 +14,7 @@ const instance_2 = [
 
 const instance_3 = [
   ["11101", "0110"],
-  ["110","1"],
+  ["110", "1"],
   ["1", "1011"],
 ];
 
@@ -24,15 +24,9 @@ function merge_and_shrink(d1, d2) {
   let t = d1[0] + d2[0];
   let b = d1[1] + d2[1];
   if (t.startsWith(b)) {
-    return [
-      t.slice(b.length),
-      ""
-    ];
+    return [t.slice(b.length), ""];
   } else if (b.startsWith(t)) {
-    return [
-      "",
-      b.slice(t.length),
-    ];
+    return ["", b.slice(t.length)];
   } else {
     return undefined;
   }
@@ -42,7 +36,6 @@ function solve_pcp(dominos, budget) {
   const result = [];
 
   function recursion_step(difference, budget) {
-    const [t, b] = difference;
     if (budget <= 0) {
       return false;
     }
@@ -66,7 +59,7 @@ function solve_pcp(dominos, budget) {
     return false;
   }
 
-  recursion_step(["",""], budget);
+  recursion_step(["", ""], budget);
 
   return result.reverse();
 }
@@ -83,52 +76,50 @@ function iterate_search_space(dominos, start_budget, end_budget, incrementer) {
 }
 
 function print_solution(dominos, solution) {
-  const dom_s = dominos.map((domino, index) =>
-        `${index + 1}:(${domino[0]}, ${domino[1]})`).join(" ");
+  const dom_s = dominos
+    .map((domino, index) => `${index + 1}:(${domino[0]}, ${domino[1]})`)
+    .join(" ");
   if (!Array.isArray(solution) || solution.length == 0) {
     console.log("No solution for dominos found " + dom_s);
     return;
-  } 
+  }
   console.log("Found solution of length " + solution.length);
   console.log("Dominos  > " + dom_s);
-  console.log("Solution > " + solution.map(i => i + 1).join(","));
-  console.log("Top      > " + solution.map(i => dominos[i][0]).join(""));
-  console.log("Bottom   > " + solution.map(i => dominos[i][1]).join(""));
+  console.log("Solution > " + solution.map((i) => i + 1).join(","));
+  console.log("Top      > " + solution.map((i) => dominos[i][0]).join(""));
+  console.log("Bottom   > " + solution.map((i) => dominos[i][1]).join(""));
 }
 
 function parseOptions() {
-  const options = process.argv.slice(2).map(s => parseInt(s, 10)).filter(n => n > 0);
-  if (options.length == 0
-    || options[0] > 3
-    || options[1] == undefined) {
+  const options = process.argv
+    .slice(2)
+    .map((s) => parseInt(s, 10))
+    .filter((n) => n > 0);
+  if (options.length == 0 || options[0] > 3 || options[1] == undefined) {
     return undefined;
   }
-  const [instance, start_budget, end_budget, incrementer ] = options
+  const [instance, start_budget, end_budget, incrementer] = options;
   return {
     dominos: instances[instance - 1],
     instance,
     start_budget: start_budget,
     end_budget: end_budget ?? start_budget,
-    incrementer: incrementer ?? 1
-  }
+    incrementer: incrementer ?? 1,
+  };
 }
 
 function processOptions(options) {
   if (options == undefined) {
-    console.log("Usage: node pcp.js [instance 1,2,3] [start_budget int] [end_budget int] [incrementer int]");
+    console.log(
+      "Usage: node pcp.js [instance 1,2,3] [start_budget int] [end_budget int] [incrementer int]"
+    );
     return;
   }
   const { dominos, start_budget, end_budget, incrementer } = options;
   print_solution(
     dominos,
-    iterate_search_space(
-      dominos,
-      start_budget,
-      end_budget,
-      incrementer));
+    iterate_search_space(dominos, start_budget, end_budget, incrementer)
+  );
 }
 
-const options = parseOptions();
 processOptions(parseOptions());
-
-
